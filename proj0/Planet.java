648ab2e95d8877758b6757d430a785c3860c6292
unknown <erenoytuner@gmail.com>
2025-05-30 18:33:01 +0200

    1st change to get full

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -4,7 +4,7 @@ public class Planet{
 	public double xxVel, yyVel;
 	public double mass;
 	public String imgFileName;
-	public static double grav= 6.67e-11; 
+	private static double grav= 6.67e-11; 
 	public Planet(double xP, double yP, double xV,
 				double yV, double m, String img)
 	{
